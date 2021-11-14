d16424500a029cb20cafe81d9359d5de3b77d34d
Eduardo - Sedue - Rey <proyectos@seduerey.com>
2021-11-14 21:59:49 +0100

    PEC 2 Asignatura. Much ado about nothing

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,14 +3,27 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+ import * as bootstrap from 'bootstrap';
 
 /**
  * Write any other JavaScript below
  */
 
+
 +( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
+  // const university = "UOC";
+  // console.log(`Hello, ${university}!`);
+  document.getElementById('seatsoccupied')?.innerText =
+        document.querySelectorAll('.seat__check[disabled]').length;
+  Array.from(document.getElementsByClassName('seat__check')).map(
+    (el) => el.addEventListener('change', () => {
+      const checkeds = document.querySelectorAll('.seat__check:checked').length;
+      document.getElementById('seatsnumber').innerText = checkeds;
+      if (checkeds > 0) {
+        document.getElementById('buytickets').removeAttribute('disabled');
+      } else {
+        document.getElementById('buytickets').setAttribute('disabled', 'disabled');
+      }
+    })
+  );
 } )();
